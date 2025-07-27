463bc1adb91b3a16e4bc386175aae43932e13b97
Henrique Cardoso Chagas <henriquecardoso03321@gmail.com>
2025-07-27 10:16:57 -0300

    Amigo Secreto

--- README.md
+++
@@ -1,2 +0,0 @@
-# challenge-amigo-secreto_pt-main
-
--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigos= document.querySelector('input').value;
+    console.log(nomeAmigos);
+
+    if (nomeAmigos == ""){
+        //console.log ('Por favor, insira um nome.');
+        alert ('Por favor, insira um nome.');
+    }else {
+        amigos.push(nomeAmigos);
+        console.log(amigos);
+        limparCampo();
+        return nomeAmigos;
+    }
+    
+}
+   for (let i=0;i<amigos.length;i++){
+        const item = document.createElement('li');
+        item.textContent = amigos[i];
+        nomeAmigos.appenchild(item);
+    }
+
+function sortearAmigo(){
+    
+ }
+
+function limparCampo() {
+    nomeAmigos = document.querySelector('input');
+    nomeAmigos.value = '';
+}
